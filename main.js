a04faef3fe24918a71c333e50393a0023deb655d
Grant Kayes <glk25@georgetown.edu>
2019-12-01 17:28:05 -0600

    Redesign

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const menu = document.getElementById('menu');
+const body = document.body;
 
 menu.addEventListener('click', openMenu);
 
@@ -6,6 +7,7 @@ function openMenu() {
   console.log("clicked!");
   if (menu.classList.contains("card-one")) {
     menu.classList.replace("card-one", "card-one-translucent");
+    body.classList.add('dark');
   } else {
     menu.classList.replace("card-one-translucent", "card-one");
   }
