e5d55974e434fea3c3e64cbcf942d8429516871c
Grant Kayes <glk25@georgetown.edu>
2019-12-12 02:20:31 -0600

    Update

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 const menu = document.getElementById('menu');
 const element = document.getElementById('drop');
-console.log(element);
+const element2 = document.getElementById('drop2');
+const element3 = document.getElementById('drop3');
 const body = document.body;
 
-menu.addEventListener('click', openMenu);
 element.addEventListener('mouseover', function () {
   element.classList.add('drop');
 });
 
 element.addEventListener('mouseout', function () {
   element.classList.remove('drop');
+});
+
+element2.addEventListener('mouseover', function () {
+  element2.classList.add('drop');
+});
+
+element2.addEventListener('mouseout', function () {
+  element2.classList.remove('drop');
+});
+
+element3.addEventListener('mouseover', function () {
+  element3.classList.add('drop');
+});
+
+element3.addEventListener('mouseout', function () {
+  element3.classList.remove('drop');
 });
 
 function openMenu() {
