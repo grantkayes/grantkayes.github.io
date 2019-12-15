c7c501aa88e20db2d64481ff68f829c21f441ff1
Grant Kayes <glk25@georgetown.edu>
2019-12-14 21:44:38 -0600

    Add pretty colors

--- main.js
+++ main.js
@@ -1,31 +1,43 @@
 const menu = document.getElementById('menu');
+const aboutMe = document.getElementById('about');
+const nav = document.getElementById('nav');
+const experience = document.getElementById('experience');
 const element = document.getElementById('drop');
 const element2 = document.getElementById('drop2');
 const element3 = document.getElementById('drop3');
 const body = document.body;
+const colorClasses = ['two', 'three', 'four'];
 
-element.addEventListener('mouseover', function () {
-  element.classList.add('drop');
-});
-
-element.addEventListener('mouseout', function () {
-  element.classList.remove('drop');
-});
-
-element2.addEventListener('mouseover', function () {
-  element2.classList.add('drop');
-});
-
-element2.addEventListener('mouseout', function () {
-  element2.classList.remove('drop');
-});
-
-element3.addEventListener('mouseover', function () {
-  element3.classList.add('drop');
-});
-
-element3.addEventListener('mouseout', function () {
-  element3.classList.remove('drop');
+menu.addEventListener('click', function () {
+  if ([...this.classList].includes('two')) {
+    this.classList.remove('two');
+    aboutMe.classList.remove('two');
+    nav.classList.remove('two');
+    experience.classList.remove('two');
+    this.classList.add('three');
+    aboutMe.classList.add('three');
+    nav.classList.add('three');
+    experience.classList.add('three');
+  } else if ([...this.classList].includes('three')) {
+    this.classList.remove('three');
+    aboutMe.classList.remove('three');
+    nav.classList.remove('three');
+    experience.classList.remove('three');
+    this.classList.add('four');
+    aboutMe.classList.add('four');
+    nav.classList.add('four');
+    experience.classList.add('four');
+  } else if ([...this.classList].includes('four')) {
+    this.classList.remove('four');
+    aboutMe.classList.remove('four');
+    nav.classList.remove('four');
+    experience.classList.remove('four');
+  } else {
+    this.classList.add('two');
+    aboutMe.classList.add('two');
+    nav.classList.add('two');
+    experience.classList.add('two');
+  }
 });
 
 function openMenu() {
